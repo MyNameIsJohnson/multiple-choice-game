b54b3e0ac6bd77d515a3c719df522cb85cef67b1
johnson <johnsonle2929@gmail.com>
2022-06-16 14:20:27 -0700

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -107,58 +107,6 @@ const App = () => {
   return (
     <>
       <GlobalStyle />
-<<<<<<< HEAD
-      <Wrapper className="App">
-        <h1>Let's See How Much You Know</h1>
-        {/* Conditional rendering */}
-        {/* Only show button if gameOver OR userAnswers length is equal to TOTAL_QUESTIONS else render null*/}
-        {gameOver || userAnwers.length === TOTAL_QUESTIONS ? (
-          <button className="start" onClick={startQuiz}>
-            Start
-          </button>
-        ) : null}
-
-        {/* Only show score if not gameOver else render null */}
-        {!gameOver ? <p className="score">Score: {score} </p> : null}
-
-        {/* Only show if loading  */}
-        {loading && (
-          <p className="loadingQuestions">
-            <svg
-              id="svg-spinner"
-              xmlns="http://www.w3.org/2000/svg"
-              width="48"
-              height="48"
-              viewBox="0 0 48 48"
-            >
-              <circle cx="24" cy="4" r="4" fill="#fff" />
-              <circle cx="12.19" cy="7.86" r="3.7" fill="#fffbf2" />
-              <circle cx="5.02" cy="17.68" r="3.4" fill="#fef7e4" />
-              <circle cx="5.02" cy="30.32" r="3.1" fill="#fef3d7" />
-              <circle cx="12.19" cy="40.14" r="2.8" fill="#feefc9" />
-              <circle cx="24" cy="44" r="2.5" fill="#feebbc" />
-              <circle cx="35.81" cy="40.14" r="2.2" fill="#fde7af" />
-              <circle cx="42.98" cy="30.32" r="1.9" fill="#fde3a1" />
-              <circle cx="42.98" cy="17.68" r="1.6" fill="#fddf94" />
-              <circle cx="35.81" cy="7.86" r="1.3" fill="#fcdb86" />
-            </svg>
-          </p>
-        )}
-
-        {/* Only show card if not loading && not gameOVer && */}
-        {!loading && !gameOver && (
-          <QuestionCard
-            questionNumber={number + 1}
-            totalQuestions={TOTAL_QUESTIONS}
-            question={questions[number].question}
-            answers={questions[number].answers}
-            userAnswer={userAnwers ? userAnwers[number] : undefined}
-            callback={checkAnswer}
-          />
-        )}
-
-        {/* Only show button if not gameOver && not loading && userAnswers length is equal to number + 1 && number not equal TOTAL_QUESTIONS - 1, else render null */}
-=======
       <Wrapper>
         <h1>React Quiz</h1>
 
@@ -182,25 +130,15 @@ const App = () => {
             userAnswer={userAnwers ? userAnwers[number] : undefined}
           />
         )}
->>>>>>> 5a4cc287be177f2ba35e975e617f5716db351705
         {!gameOver &&
         !loading &&
         userAnwers.length === number + 1 &&
         number !== TOTAL_QUESTIONS - 1 ? (
-<<<<<<< HEAD
           <>
             <button className="next" onClick={nextQuestion}>
               Next Question
             </button>
           </>
-<<<<<<< HEAD
-=======
-          <button className="next" onClick={nextQuestion}>
-            Next Question
-          </button>
->>>>>>> 5a4cc287be177f2ba35e975e617f5716db351705
-=======
->>>>>>> 93323c46881635a0f0921f585e054e113a449801
         ) : null}
       </Wrapper>
     </>
